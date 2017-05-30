90242faf7145c5e5dceeca0e8d74cfb5c7a3c301
Jannik <kimjannikleonhardt@yahoo.de>
2017-05-30 17:12:53 +0200

    index.php UTF-Fähig

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <html>
+<head><meta charset="UTF-8"><title>Glotzkowski</title>
+<body>
 <h1>Beispielüberschrift</h1>
 <p>Dies ist ein Testparagraph. Er dient dazu zu überprüfen ob die Datei Index.php erreichbar ist, denn wenn sie es ist, dann sieht man diesen text</p>
+</body>
 </html>
