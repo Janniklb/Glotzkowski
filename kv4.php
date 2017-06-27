cc18cf719a696575ec6588e07aca84cf0a191259
Janniklb <kimjannikleonhardt@yahoo.de>
2017-06-27 09:00:41 +0200

    error gefoxed, der eine Schleife unterbrochen hat

--- kv4.php
+++ kv4.php
@@ -84,104 +84,97 @@ do{
 		$mini_array = $$mini_array_speicher_name;
 	}	
 }while($ende);
-//----------------------------------------------------------------- LETZTE MINIMIERUNG ---------------------------------------------------------------------------
-
+//----------------------------------------------------------------- LETZTE MINIMIERUNG----------------------------------------------------------
 //Ab hier verwende ich den $mini_array von Joschka
-//Im obigen Teil hat bereits eine minimierung stattgefunden. Es wurden jeweils einzelne Teile der funktion minimiert.
+//Im obigen Code hat bereits eine minimierung stattgefunden. Zusammen minimierte Zahlen wurden in einem Array gespeichert
 //Bsp. für ein Teil:	Ganzes: S1S3S4 v !S6S3S9 ein Teil wäre dann S1S3S4
 //Jedes Teil hat ein eigenes unterarray im $mini_array, in dem die optimierten Zahlen dieses arrays gespeichert sind Bsp: 0,3,6,7 => Die Kombination der Eingänge in der Wahrheitstabelle in der Zeile 0,3,6 und 7 ergeben 1
 
-//später verwendete Variablen deklarieren
-$einshoch = 0; 
+//Variablen deklarieren
+$einshoch = 0;
 $count = 0;
 $countx=0;
 $zuzaehlen=0;
+$zaehler = 0;
+$minzahl = 0;
 $vergleichsbasis = 0;
 $explodespeicher = 0;
 $countspeicher = 0;
 
-//später verwendete arrays deklarieren
+//Arrays deklarieren
 $deczahlenx = array();
 $deczahlen = array();
 $vergleichswerte = array();
-
-
-//Zählt wie viele Teile die boolsche Funktionen in der Endformel hat (Vor der Aussortierung) und speichert die zahl in $count. !S1S2 v !S3S4 == 2 teile
+	
+//Zählt wie viele Teile die Funktionen in der Endformel haben würde (Vor der kommenden optimierung)
 $count = count($mini_array);
 
-//Die folgende Schleife zählt wie viele Zahlen in den jeweiligen Teilen optimiert wurden und fügt diese Zahl in das dazugehörige unterarray ein
+
+//Diese Schleife zählt wie viele Zahlen pro Teil optimiert wurden. Die Info wird mit einer Identifikationsnummer für das Teil an den Mini_array angehängt
 for($i=0; $i<$count; $i++){
-	$zuzaehlen = explode(":", $mini_array[$i]["deczahl"]);		//Nimmt die verwendeten Zahlen und speichert jede als einzelnen Eintrag im $zuzahlen array
-	$countx = count($zuzaehlen);								//Zählt wie viele Zahlen zusammen minimiert wurden
-	$mini_array[$i]["deczahlen"] = $countx;						//Speichert die menge der optimierten Zahlen zusätzlich im Datenarray unter dem Key "deczahlen"
-	array_push($deczahlenx, $i, $countx);						//pusht die Nummer des verwendeten Datenarrays und die Menge der deczahlen dieses Datenarrays in deczahlenx
-	array_push($deczahlen, $deczahlenx);						//pusht deczahlenx mit den infos in den deczahlen array
-	$deczahlenx=array(); 										//leert den deczahlenx array für die benutzung um nächsten durchlauf
+	$zuzaehlen = explode(":", $mini_array[$i]["deczahl"]); 
+	$countx = count($zuzaehlen); 
+	$mini_array[$i]["deczahlen"] = $countx; 
+	array_push($deczahlenx, $i, $countx); 
+	array_push($deczahlen, $deczahlenx); 
+	$deczahlenx=array(); 
 }
 
 
-//Öffnen der großen Schleife, die bis zum Ende der ganzen Aussortierung läuft und alles umfasst
+//Hier wird eine Schleife geöffnet, die die ganze optimierung umfasst und läuft bis alle Teile mit den anderen verglichen wurden
+//Teile, deren optimierte Zahlen auch in anderen vorkommen, werden aussortiert
 for($e=0; $e<$count+1; $e++){
-
-//Die folgende Schleife guckt welche Boolsche funktion in der ersten Aussortierung die wenigsten Zahlen minimiert hat
-
-	for($i=0; $i<$count; $i++){			//Öffnen der ersten Schleife, dessen $i später den Zähler für die Vergleichsbasis darstellt
-		$zaehler = 0;					//2 Variabeln für die kommende Schleife deklariert
-		$minzahl = 0;
-			for($a=0; $a<$count; $a++){		//Öffnen der zweiten Schleife, dessen $a später den Zähler für die Vergleichswerte darstellt
-				if($deczahlen[$i][1] <= $deczahlen[$a][1] && $mini_array[$deczahlen[$i][0]]["benutzt"]==0){			//Wenn die Anzahl an minimierten zahlen bei der Vergleichsbais kleiner oder gleich der Anzahl beim Vergleichswert ist, dann wird der Zähler um 1 erhöht
-					$zaehler++;
-					$minzahl = $i; 			//speichert welcher verglichene array verwendet wurde. Ist immer der / einer der Array/s mit den wenigsten minimierten zahlen
-				}
 	
-				if($zaehler==$count-$einshoch && $mini_array[$deczahlen[$i][0]]["benutzt"]==0){ 	//Wenn der Zähler genauso hoch ist wie die Anzahl der unverglichenen arrays (Alles wurde verglichen), dann geh aus der Schleife
-					break;
-				}
+	//In der folgenden Doppelschleife wird überprüft, in welchem / welchen Teil der Formel die wenigsten Zahlen optimiert wurden.
+	for($i=0; $i<$count; $i++){
+		for($a=0; $a<$count; $a++){
+			if($deczahlen[$i][1] <= $deczahlen[$a][1] && $mini_array[$deczahlen[$i][0]]["benutzt"]==0){
+			$zaehler++;
+			$minzahl = $i;					//Hier wird die Indentifikationsnummer des Arrays mit den wenigsten optimierten Zahlen gespeichert
 			}
-		if($zaehler==$count-$einshoch && $mini_array[$deczahlen[$i][0]]["benutzt"]==0){			//Das selbe wie ein IF weiter oben, um aus beiden Schleifen für den vergleich zu kommende
+	
+			if($zaehler==$count-$einshoch && $mini_array[$deczahlen[$i][0]]["benutzt"]==0){
+				break;
+			}
+		}
+		if($zaehler==$count-$einshoch && $mini_array[$deczahlen[$i][0]]["benutzt"]==0){
 			break;
 		}
 	}
-
 	
-	$arrminzahl = $deczahlen[$minzahl][0]; // In $arrminzahl wird die Nummer des Arrays mit der kleinsten Anzahl an minimierten Zahlen gespeichert
-	$vergleichsbasis = explode(":", $mini_array[$arrminzahl]["deczahl"]);	//Die im Array $mini_array gespeicherte Information welche Zahlen bis dorthin genau optimiert wurden, wird im Array $vergleichsbasis gespeichert (Jede Zahl als eigener Eintrag). Diese Zahlen werden gleich mit allen anderen Verglichen
 
-//In der folgenden Schleife werden alle anderen minimierten Zahlen in einem Zweiten array gespeichert um mit $vergleichsbasis verglichen werden zu können
-
-	for($i=0;$i<$count;$i++){ //Eine Schleife wird geöffnet, damit man mit $i wieder durch alle Arrays zählen kann
-		if($i != $arrminzahl && ($mini_array[$i]["benutzt"]==0 || $mini_array[$i]["benutzt"]==2)){	//Alle anderen optimierungen (außer die in $vergleichsbasis und welche, die schon verglichen und aussortiert wurden), werden in der nächsten schleife verarbeitet
-			$explodespeicher = explode(":", $mini_array[$i]["deczahl"]);		//Die minimierten Zahlen der arrays werden im explodespeicher gespeichert
-			$countspeicher = count($explodespeicher);							//In Countspeicher wird gespeichert, wie viele minimierte zahlen in $explodespeicher sind
-		
-			for($a=0; $a<$countspeicher; $a++){									//Die Schleife pusht alle in $explodespeicher enthaltenen, minimierten Zahlen in den $vergleichswerte array
+//Mit der Information in welchem Teil die wenigsten Zahlen optimiert wurden, werden eben diese Zahlen herausgefunden und in einem extra array gespeichert
+	$arrminzahl = $deczahlen[$minzahl][0];
+	$vergleichsbasis = explode(":", $mini_array[$arrminzahl]["deczahl"]);
+	$vergleichswerte = array();
+	
+	
+//Die optimierten Zahlen aller anderen Teile werden in einem Zweiten array gespeichert
+	for($i=0;$i<$count;$i++){
+		if($i != $arrminzahl && ($mini_array[$i]["benutzt"]==0 || $mini_array[$i]["benutzt"]==2)){
+		$explodespeicher = explode(":", $mini_array[$i]["deczahl"]);
+		$countspeicher = count($explodespeicher);
+			for($a=0; $a<$countspeicher; $a++){
 				array_push($vergleichswerte, $explodespeicher[$a]);
 			}
 		}
-		$countspeicher = 0;														//Countspeicher und Explodespeicher werden für die wiederverwendung der Schleife geleert
-		$explodespeicher = 0;
+	$countspeicher = 0;
+	$explodespeicher = 0;
 	}
 
-
-//Wenn jede der minimierten Zahlen in der Vergleichsbasis auch in den Verlgeichswerten vorkommt, dann wird der Unterwert "benutzt" im Array aus dem die Vergleichsbasis kommt =1 gesetzt. 1 Gesetze Arrays sind aussortiert
-	if(array_intersect($vergleichsbasis, $vergleichswerte) == ($vergleichsbasis)){
-		$mini_array[$arrminzahl]["benutzt"]=1;
-		$einshoch++; //Da ab jetzt ein array weniger verglichen werden muss, wird $einshoch inkrementiert um die Vergleichsschleife weiter oben jedes mal kleiner zu machen
-	}
-
-
-//Wenn nicht alle Zahlen der vergleichsbasis in den Vergleichswerten vorkommen, dann muss diese optimierung verwendet werden, sie wird mit 2 in "benutzt" markiert. Mit 2 markierte Arrays werden in der Finalen, minimierten Formel verwendet
-	else{
-		$mini_array[$arrminzahl]["benutzt"]=2;
-		$einshoch++; //Da ab jetzt ein array weniger verglichen werden muss, wird $einshoch inkrementiert um die Vergleichsschleife weiter oben jedes mal kleiner zu machen
-	}
-
-} //Schluss der großen Schleife
-
-
-for($i=0; $i<$count; $i++){
+//Nachfolgend werden die beiden Arrays verglichen. Wenn alle Zahlen des einzelnen Teils irgendwo in den anderen Teilen vorkommen, wird das einzelne Teil nicht mehr benötigt
+//Ist nur eine Zahl des einzelen Teils einzigartig, dann wird das Teil für die Endformel übernommen
 	
+	if(array_intersect($vergleichsbasis, $vergleichswerte) == ($vergleichsbasis)){
+		$mini_array[$arrminzahl]["benutzt"]=1;	//Nicht einzigartige Teile werden mit 1 markiert und somit nicht in der finalen Formel verwendet
+		$einshoch++;
+	}
+	else{
+		$mini_array[$arrminzahl]["benutzt"]=2;	//Zahlen, welche eine einzigartige Zahl haben, werden mit 2 markiert und für die letzte Formel übernommen
+		$einshoch++;
+	}
 }
+
 $formel = "";
 foreach( $mini_array as $key => $wert ){
 	if( $mini_array[ $key ][ "benutzt" ] == 2 ){
